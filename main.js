abccc3281594f503f3eef9eec5658c8ccf7a465b
sYStemOutput <sadaatyaseen.personal@gmail.com>
2022-12-31 22:58:57 -0800

    Final changes to the website overall.

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
 	
-	var buttonWidth = $("span.discordButton").css('width');
-	var padLeft = $("span.discordButton").css('padding-left');
-	$("span.discordButton").on({
-		mouseenter: function() {
-			$(this).animate({
-				'border-radius': '25px',
-				'width': '250px',
-				'padding-left': '30px'
-			}, 200);
-		},  
-		mouseleave: function() {
-			$(this).animate({
-				opacity: '1',
-				'border-radius': '0px',
-				'width': buttonWidth,
-				'padding-left': padLeft
-			}, 400);
-		}, 
-		click: function() {
-			$(this).animate({
-				opacity: '1',
-				'width': buttonWidth
-			});
-		}  
-	});
-  });
+var buttonWidth = $("span.discordButton").css('width');
+var padLeft = $("span.discordButton").css('padding-left');
+$("span.discordButton").on({
+	mouseenter: function() {
+		$(this).animate({
+			'border-radius': '25px',
+			'width': '250px',
+			'padding-left': '30px'
+		}, 200);
+	},  
+	mouseleave: function() {
+		$(this).animate({
+			opacity: '1',
+			'border-radius': '0px',
+			'width': buttonWidth,
+			'padding-left': padLeft
+		}, 400);
+	}, 
+	click: function() {
+		$(this).animate({
+			opacity: '1',
+			'width': buttonWidth
+		});
+	}  
+});
+});
