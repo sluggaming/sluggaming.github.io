9fdc434305aa7fc2354a7c32ef7e21a96d5e260b
Jeffuz <jeffzhang0049@gmail.com>
2024-01-09 21:30:08 -0800

    update valorant blue and gold team

--- src/pages/games/Valorant.js
+++ src/pages/games/Valorant.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import valorantTeamData from '../../data/Valorant Team.json';
 import TeamCard from '../../components/Teams Page/TeamCard';
+import valorantTeamBData from '../../data/Valorant Team B.json';
 
 const Valorant = () => {
     return (
@@ -22,7 +23,8 @@ const Valorant = () => {
                     />
                 </div>
             </section>
-                <section className='pt-80 font-Montserrat'>
+            <section className='pt-80 font-Montserrat'>
+                <section>
                     <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
                         UCSC Gold
                     </div>
@@ -40,6 +42,28 @@ const Valorant = () => {
                         </div>
                     </div>
                 </section>
+                <section>
+                    <div className='flex justify-center sm:text-5xl text-3xl text-[#00588F]'>
+                        UCSC Blue
+                    </div>
+                    <div className='flex justify-center max-[1100px]:px-[50px] pt-3'>
+                        <img src={require('../../images/Achivements Page/Yellow Line.png')} alt='Yellow Line' loading="lazy" />
+                    </div>
+                    <div className='min-[1100px]:px-[215px] px-[50px] text-[25px] pt-[30px] text-center'>
+                        University of California, Santa Cruz | Valorant Junior Varsity Team
+                    </div>
+                    <div className='container mx-auto py-12 px-8 '>
+                        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6'>
+                            {valorantTeamBData.map((teamMember) => (
+                                <TeamCard key={teamMember.id} {...teamMember} />
+                            ))}
+                        </div>
+                    </div>
+                </section>
+                <div id='credit' className='flex justify-center pb-12'>
+                    pc:@konceptsbykap
+                </div>
+            </section>
         </div>
     )
 }
