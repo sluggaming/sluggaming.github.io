f1b83b474e01bca53a85f64ef31dfa58b9c14b07
Jeffuz <jeffzhang0049@gmail.com>
2024-01-09 21:46:41 -0800

    switch to browser routing

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
-// import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    {/* <BrowserRouter basename='sluggaming'> */}
+
+    <BrowserRouter>
     <App />
-    {/* </BrowserRouter> */}
-  </HashRouter>
+    </BrowserRouter>
+
 
 );
 
